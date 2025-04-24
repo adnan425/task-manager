22ee502b7d45e6a3b3e8c5497629baec14571a34
adnan425 <166127663+adnan425@users.noreply.github.com>
2025-04-25 00:05:25 +0400

    Header Extracted

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,24 @@
 "use client";
+import Header from "@/components/Header";
 import TableList from "@/components/tasks/TableList";
 
 export default function TaskTable() {
   return (
-    <div className="flex justify-center min-h-screen flex justify-center items-start py-10 px-4">
-      <div className="w-full max-w-6xl px-4 overflow-x-auto ">
-        <h1 className="scroll-m-20 text-3xl sm:text-4xl lg:text-5xl font-extrabold tracking-tight text-center">
-          Your Tasks
-        </h1>
-        <p className="leading-7 mt-4 text-muted-foreground text-center max-w-2xl mx-auto px-4">
-          View, filter, and manage your personal tasks. Sort by priority or status, and keep track of what matters most.
-        </p>
-        <TableList />
+    <>
+      <Header />
+      <div className="flex justify-center min-h-screen flex justify-center items-start py-10 px-4">
+        <div className="w-full max-w-6xl px-4 overflow-x-auto ">
+          <h1 className="scroll-m-20 text-3xl sm:text-4xl lg:text-5xl font-extrabold tracking-tight text-center">
+            Your Tasks
+          </h1>
+          <p className="leading-7 mt-4 text-muted-foreground text-center max-w-2xl mx-auto px-4">
+            View, filter, and manage your personal tasks. Sort by priority or status, and keep track of what matters most.
+          </p>
+          <TableList />
+        </div>
       </div>
-    </div>
+    </>
+
 
   );
 }
